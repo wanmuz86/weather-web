737e432d1c0a4281283a23546b49a282507fc5a4
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-09-07 21:53:33 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,57 @@
     <h1>Weather website</h1>
     <div id="search" class="card px-5 py-5">
       <input type="text" name="search" 
-      placeholder="Search the weather" class="form-control mb-3">
+      placeholder="Search the weather" class="form-control mb-3" v-model="searchedCity">
       <button class="btn btn-primary" v-on:click="searchWeather">Search</button>
     </div>
-    <div id="weather">
+    <div id="weather" v-if="weathers.length != 0">
       <h2>Weather info</h2>
+      <h3>{{city}}</h3>
       <table class="table table-bordered table-striped">
         <tr>
-          <th>Date time</th>xx
+          <th>Date time</th>
           <th>Weather</th>
           <th>Temperature</th>
           <th>Icon</th>
         </tr>
+        <tr v-for="weather in weathers" :key="weather.dt" v-on:click="selectWeather(weather)">
+          <td>{{new Date(weather.dt*1000)}}</td>
+          <td>{{weather.weather[0].main}}</td>
+          <td>{{(weather.temp.day - 273.15).toFixed(2)}} C</td>
+          <td><img v-bind:src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"></td>
+        </tr>
       </table>
+    </div>
+    <div id="detail" class="card px-5 py-5" v-if="selectedWeather">
+      <p><img v-bind:src="`https://openweathermap.org/img/wn/${selectedWeather.weather[0].icon}@2x.png`"/></p>
+      <p>{{new Date(selectedWeather.dt*1000)}}</p>
     </div>
   </div>
 </template>
-
 <script>
 
   export default {
     name: 'App',
     data() {
       return {
-      weathers: []
+      weathers: [],
+      city: null,
+      searchedCity:'',
+      selectedWeather:null
     }
     },
     methods: {
+      selectWeather: function(weather){
+        console.log(weather)
+        this.selectedWeather = weather;
+      },
       searchWeather: function(){
-        fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=Kuala%20Lumpur&appid=9fd7a449d055dba26a982a3220f32aa2')
+        fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q='+this.searchedCity+'&appid=9fd7a449d055dba26a982a3220f32aa2')
         .then(response => response.json())
         .then(data => {
           console.log('Success:', data);
           this.weathers = data["list"]
+          this.city = data.city.name
         })
         .catch((error) => {
           console.error('Error:', error);
